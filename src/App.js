6064b355d476d24d6ff6ed796d651f6b6bfa301b
iamfarraz <farazanwar8@gmail.com>
2021-04-21 04:09:29 +0530

    user login register connected to backend

--- src/App.js
+++ src/App.js
@@ -9,14 +9,60 @@ import SignInShopkeeper from './components/SignInShopkeeper/SignInShopkeeper'
 import RegisterBuyer from './components/RegisterBuyer/RegisterBuyer'
 import RegisterShopkeeper from './components/RegisterShopkeeper/RegisterShopkeeper'
 
+
+
+const intialState={
+  route:'usertype',
+  isSignedin:'false',
+  user:{   
+           cust_id:'',
+           username:'',
+            mail:'',
+            password:'',
+            phone:'',
+            houseno:'',
+            area:'',
+            city:'',
+            pincode:''
+
+  }
+
+}
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      route: 'usertype',
-    }
+    this.state = {intialState }
 
   }
+
+
+//   area: "frstpong"
+// city: "fist"
+// cust_id: 1684
+// houseno: 24
+// locality_pin_code: 453670
+// mail: "frst@gmail.com"
+// password: "$2b$10$g9OyN1iytIw.LHe0fwkcwu41orIAy4sn8W.Hr8pBWMBkCLtFwglzS"
+// phone: "90909087"
+// sector: null
+// username: "frstuer"
+
+  loadUser=(user)=>{
+    // console.log(user)
+    console.log("inside",user.cust_id) 
+    this.setState({user:{ 
+      cust_id:user.cust_id,
+      username:user.username,
+      mail:user.mail,
+      password:user.password,
+      phone:user.phone,
+      houseno:user.houseno,
+      area:user.area,
+      city:user.city,
+      pincode:user.locality_pin_code }})
+ 
+  }
+
   onRouteChange = (route) => {
     
     this.setState({route: route});
@@ -37,19 +83,28 @@ class App extends Component {
       route === 'usertype' ?
           <UserType onRouteChange={this.onRouteChange}/>
         :  (route === 'signinbuyer' ?
-          <SignInBuyer onRouteChange={this.onRouteChange}/>
-          : (route === 'signinshopkeeper' ?
-          <SignInShopkeeper onRouteChange={this.onRouteChange}/>
-          : (route === 'registerbuyer' ?
-          <RegisterBuyer onRouteChange={this.onRouteChange}/>
-          : (route === 'registershopkeeper' ?
-          <RegisterShopkeeper onRouteChange={this.onRouteChange}/>
-          : <div className='center pa7-ns'>
-              <img src={Construction} alt='Logo' />  
-          </div>
+          <SignInBuyer onRouteChange={this.onRouteChange} loadUser={this.loadUser} />
+              : (route === 'signinshopkeeper' ?
+              <SignInShopkeeper onRouteChange={this.onRouteChange}/>
+                  : (route === 'registerbuyer' ?
+                  <RegisterBuyer onRouteChange={this.onRouteChange} loadUser={this.loadUser} />
+                                : (route === 'registershopkeeper' ?
+                                 <RegisterShopkeeper onRouteChange={this.onRouteChange}/>
+                                : (route==='home'  ?
+                               <div>
+                                 <h1>{`I am ${this.state.user.username}`}</h1>
+                                 <h2>{`I live in ${this.state.user.city}`}</h2>
+                               </div>
+                               : 
+                               <div className='center pa7-ns'>
+                                    <img src={Construction} alt='Logo' />  
+                                </div>
+                               )
+                                
+                              )
+                )
+            ) 
         )
-        )
-        ) )
       }
       
       {/*
--- src/components/SignInBuyer/SignInBuyer.js
+++ src/components/SignInBuyer/SignInBuyer.js
@@ -2,10 +2,49 @@ import React from 'react';
 
 import './SignInBuyer.css'
 
-const SignInBuyer = ({ onRouteChange }) => {
+
+    class SignInBuyer extends React.Component{
+     constructor(props){
+         super(props);
+         this.state={
+            signinMail:'',
+            signinPassword:'',
+        }
+
+     }
+     onMailChange=(event)=>{
+        this.setState({signinMail:event.target.value})
+      //   console.log(this.state.signinMail)
+  
+    }
+    onPasswordChange=(event)=>{
+      this.setState({signinPassword:event.target.value})
+    }
+    onSubmitSignin=()=>{
+        fetch("https://api-shopstop.herokuapp.com/userlogin",{
+            method:'post',
+            headers:{'Content-Type':"application/json"},
+            body:JSON.stringify({
+              mail:this.state.signinMail,
+              password:this.state.signinPassword,
+            })
+        })
+        .then(res=>res.json())
+            .then(user=>{
+             
+                if(user.cust_id){
+                    console.log("user main hu", user)
+                    this.props.loadUser(user)
+                    this.props.onRouteChange('home')
+                }
+            })
+            .catch(console.log)
+
+    }
 
 
-    return (
+        render(){
+   return (
 
         <div >
 
@@ -17,26 +56,29 @@ const SignInBuyer = ({ onRouteChange }) => {
                             <h1 className="b ma0 f3">Sign In Buyer</h1>
                         </header>
                         <article id="register-buyer" action="" className="ph3 pt3 pb4 f7">
-                            <div className="mb3">
-                                <label for="" className="db ttu b lh-copy">Username</label>
-                                <input name="username" type="text" className="input-reset w-100 mw-100 bn br1 pa2 bg-light-gray" />
-                            </div>
-                            
-                            <div className="mb4">
-                                <label for="" className="db ttu b lh-copy">Password</label>
-                                <input name="password" type="password" className="input-reset w-100 mw-100 bn br1 pa2 bg-light-gray" />
-                            </div>
+                        <div className="mb3">
+                            <label htmlFor="" className="db ttu b lh-copy">Mail</label>
+                            <input
+                              onChange={this.onMailChange}
+                             name="mail" type="mail" className="input-reset w-100 mw-100 bn br1 pa2 bg-light-gray" />
+                        </div>
+                        <div className="mb4">
+                            <label htmlFor="" className="db ttu b lh-copy">Password</label>
+                            <input 
+                              onChange={this.onPasswordChange}
+                            name="password" type="password" className="input-reset w-100 mw-100 bn br1 pa2 bg-light-gray" />
+                        </div>
                             <div className="mb3 tc f6">
                                 Are you new here? <p 
-                                onClick={() => onRouteChange('registerbuyer')}className="blue di pointer underline ph1">Register Now!</p>
+                                onClick={() =>this.props.onRouteChange('registerbuyer')}className="blue di pointer underline ph1">Register Now!</p>
                             </div>
                             <div className="mb3 tc f6">
                                 Are you a Shopkeeper? <p 
-                                onClick={() => onRouteChange('signinshopkeeper')}className="blue di pointer underline ph1">Login Here!</p>
+                                onClick={() =>this.props.onRouteChange('signinshopkeeper')}className="blue di pointer underline ph1">Login Here!</p>
                             </div>
                             <div className="tc">
                                 <input type="submit" 
-                                onClick={() => onRouteChange('underconstruction')} value="Sign In" className="ttu bn pv3 ph4 f6 bg-blue white b br-pill pointer grow" />
+                                onClick={this.onSubmitSignin} value="Sign In" className="ttu bn pv3 ph4 f6 bg-blue white b br-pill pointer grow" />
                             </div>
                         </article>
                     </div>
@@ -46,7 +88,9 @@ const SignInBuyer = ({ onRouteChange }) => {
 
         </div>
     );
-
+ 
+}
+    
 
 }
 export default SignInBuyer;
