9e351ea5b197149ce4d0efbfef1f9333146cfd54
RandomGuy1793 <faisal.alam.1999@gmail.com>
2021-04-20 23:56:56 +0530

    added user landing page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ import SignInShopkeeper from './components/SignInShopkeeper/SignInShopkeeper'
 import RegisterBuyer from './components/RegisterBuyer/RegisterBuyer'
 import RegisterShopkeeper from './components/RegisterShopkeeper/RegisterShopkeeper'
 
+import LandingUser from './components/LandingUser/LandingUser';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 class App extends Component {
   constructor() {
     super();
@@ -18,41 +22,43 @@ class App extends Component {
 
   }
   onRouteChange = (route) => {
-    
-    this.setState({route: route});
-    
+
+    this.setState({ route: route });
+
   }
 
-  
+
 
   render() {
-    const {route} = this.state;
+    const { route } = this.state;
     return (
-     <div className='App'>
-       
-      <Navigation onRouteChange={this.onRouteChange} />
-      
-      {
-      
-      route === 'usertype' ?
-          <UserType onRouteChange={this.onRouteChange}/>
-        :  (route === 'signinbuyer' ?
-          <SignInBuyer onRouteChange={this.onRouteChange}/>
-          : (route === 'signinshopkeeper' ?
-          <SignInShopkeeper onRouteChange={this.onRouteChange}/>
-          : (route === 'registerbuyer' ?
-          <RegisterBuyer onRouteChange={this.onRouteChange}/>
-          : (route === 'registershopkeeper' ?
-          <RegisterShopkeeper onRouteChange={this.onRouteChange}/>
-          : <div className='center pa7-ns'>
-              <img src={Construction} alt='Logo' />  
-          </div>
-        )
-        )
-        ) )
-      }
-      
-      {/*
+      <div className='App'>
+
+        <Navigation onRouteChange={this.onRouteChange} />
+
+        {
+
+          route === 'usertype' ?
+            <UserType onRouteChange={this.onRouteChange} />
+            : (route === 'signinbuyer' ?
+              <SignInBuyer onRouteChange={this.onRouteChange} />
+              : (route === 'signinshopkeeper' ?
+                <SignInShopkeeper onRouteChange={this.onRouteChange} />
+                : (route === 'registerbuyer' ?
+                  <RegisterBuyer onRouteChange={this.onRouteChange} />
+                  : (route === 'registershopkeeper' ?
+                    <RegisterShopkeeper onRouteChange={this.onRouteChange} />
+                    : (route === 'landinguser' ?
+                      <LandingUser onRouteChange={this.onRouteChange} />
+                      : <div className='center pa7-ns'>
+                        <img src={Construction} alt='Logo' />
+                    </div>
+                  )
+                )
+              )))
+        }
+
+        {/*
       
       
       <RegisterBuyer />
@@ -61,7 +67,7 @@ class App extends Component {
       <RegisterShopkeeper />*/}
 
 
-     </div>
+      </div>
     );
   }
 }
